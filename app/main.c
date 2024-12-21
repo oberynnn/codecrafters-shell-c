8563e2af2218a21feac3b15498acfa8090da028f
Oberyn <z5502369@ad.unsw.edu.au>
2024-12-21 17:50:09 +0700

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -5,11 +5,13 @@
 
 #define 	EXIT_0 	"exit 0"
 #define 	ECHO 	"echo "
+#define 	TYPE 	"type "
 
 // HELPER FUNCTION HEADERS
+static int driver(void);
 static void myExit(void);
 static void myEcho(char *str);
-static int driver(void);
+static void myType(char *str);
 
 // MAIN FUNCTION
 int main(void) {
@@ -17,14 +19,6 @@ int main(void) {
 }
 
 // HELPER FUNCTIONS
-static void myExit(void) {
-	exit(EXIT_SUCCESS);
-}
-
-static void myEcho(char *str) {
-	printf("%s\n", str);
-}
-
 static int driver(void) {
 	while (true) {
 		setbuf(stdout, NULL);
@@ -44,7 +38,39 @@ static int driver(void) {
 			free(buffer);
 			continue;
 		}
+		else if (strncmp(input, TYPE, 5 * sizeof(char)) == 0) {
+			char *buffer = malloc(100 * sizeof(char));
+			strcpy(buffer, input + 5);
+			myType(buffer);
+			free(buffer);
+			continue;
+		}
 		printf("%s: command not found\n", input);
 	}
 	return 0;
+}
+
+static void myExit(void) {
+	exit(EXIT_SUCCESS);
+}
+
+static void myEcho(char *str) {
+	printf("%s\n", str);
+}
+
+static void myType(char *str) {
+	char commands[][16] = {"exit", "echo", "type", "cat"};
+	int commandsSize = sizeof(commands) / sizeof(commands[0]);
+	bool found = false;
+	for (int i = 0; i < commandsSize; i++) {
+		if (strcmp(str, commands[i]) == 0) {
+			found = true;
+		}
+	}
+	if (found == true) {
+		printf("%s is a shell builtin\n", str);
+	}
+	else {
+		printf("%s: not found\n", str);
+	}
 }
