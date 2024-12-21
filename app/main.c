dd4ffbd9c9b7c462a04030f060b25af344700986
Oberyn <z5502369@ad.unsw.edu.au>
2024-12-21 17:10:08 +0700

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -1,27 +1,45 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
-int main() {
-  // Flush after every printf
-  setbuf(stdout, NULL);
-  // Uncomment this block to pass the first stage
-  printf("$ ");
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-  int inputLength = strlen(input);
-  input[inputLength - 1] = '\0';
-  printf("%s: command not found\n", input);
-  while (true) {
-    setbuf(stdout, NULL);
-    printf("$ ");
-    // Wait for user input
-    char input[100];
-    fgets(input, 100, stdin);
-    int inputLength = strlen(input);
-    input[inputLength - 1] = '\0';
-    printf("%s: command not found\n", input);
-  }
-  return 0;
+#define 	EXIT_0 	"exit 0"
+
+// HELPER FUNCTION HEADERS
+static void myExit(void);
+
+// MAIN FUNCTION
+int main(void) {
+	// Flush after every printf
+	setbuf(stdout, NULL);
+	// Uncomment this block to pass the first stage
+	printf("$ ");
+	// Wait for user input
+	char input[100];
+	fgets(input, 100, stdin);
+	int inputLength = strlen(input);
+	input[inputLength - 1] = '\0';
+	if (strcmp(input, EXIT_0) == 0) {
+		myExit();
+	}
+	printf("%s: command not found\n", input);
+	while (true) {
+		setbuf(stdout, NULL);
+		printf("$ ");
+		// Wait for user input
+		char input[100];
+		fgets(input, 100, stdin);
+		int inputLength = strlen(input);
+		input[inputLength - 1] = '\0';
+		if (strcmp(input, EXIT_0) == 0) {
+			myExit();
+		}
+		printf("%s: command not found\n", input);
+	}
+	return 0;
+}
+
+// HELPER FUNCTIONS
+static void myExit(void) {
+	exit(EXIT_SUCCESS);
 }
