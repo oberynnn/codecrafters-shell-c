85d1cecf23ca019a6fdc1ad257361fc61756dba7
Oberyn <z5502369@ad.unsw.edu.au>
2024-12-21 17:02:00 +0700

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 int main() {
   // Flush after every printf
   setbuf(stdout, NULL);
-
   // Uncomment this block to pass the first stage
   printf("$ ");
-
   // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
   int inputLength = strlen(input);
   input[inputLength - 1] = '\0';
   printf("%s: command not found\n", input);
+  while (true) {
+    setbuf(stdout, NULL);
+    printf("$ ");
+    // Wait for user input
+    char input[100];
+    fgets(input, 100, stdin);
+    int inputLength = strlen(input);
+    input[inputLength - 1] = '\0';
+    printf("%s: command not found\n", input);
+  }
   return 0;
 }
