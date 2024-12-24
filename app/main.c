85be58ef05995503d134654738e4949fc3835eaa
Oberyn <z5502369@ad.unsw.edu.au>
2024-12-24 11:48:17 +0700

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -1,23 +1,34 @@
+#include <dirent.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
+///////////////////////////////////////////////////////////////////////////////
 #define 	EXIT_0 	"exit 0"
 #define 	ECHO 	"echo "
 #define 	TYPE 	"type "
 
+///////////////////////////////////////////////////////////////////////////////
 // HELPER FUNCTION HEADERS
 static int driver(void);
 static void myExit(void);
 static void myEcho(char *str);
 static void myType(char *str);
+static bool myTypeCommandsCheck(char *str, char commands[][16], int commandsSize);
+static void myTypeCommands(char *str, char commands[][16], int commandsSize);
+static bool myTypeFileCheck(char *str);
+static void myTypeFile(char *str);
 
+///////////////////////////////////////////////////////////////////////////////
 // MAIN FUNCTION
 int main(void) {
 	return driver();
 }
 
+///////////////////////////////////////////////////////////////////////////////
 // HELPER FUNCTIONS
 static int driver(void) {
 	while (true) {
@@ -59,18 +70,89 @@ static void myEcho(char *str) {
 }
 
 static void myType(char *str) {
-	char commands[][16] = {"exit", "echo", "type", "cat"};
+	char commands[][16] = {"echo", "exit", "type"};
 	int commandsSize = sizeof(commands) / sizeof(commands[0]);
-	bool found = false;
-	for (int i = 0; i < commandsSize; i++) {
-		if (strcmp(str, commands[i]) == 0) {
-			found = true;
-		}
+	if (myTypeCommandsCheck(str, commands, commandsSize) == true) {
+		myTypeCommands(str, commands, commandsSize);
 	}
-	if (found == true) {
-		printf("%s is a shell builtin\n", str);
+	else if (myTypeFileCheck(str) == true) {
+		myTypeFile(str);
 	}
 	else {
 		printf("%s: not found\n", str);
 	}
+}
+
+static bool myTypeCommandsCheck(char *str, char commands[][16], int commandsSize) {
+	for (int i = 0; i < commandsSize; i++) {
+		if (strcmp(str, commands[i]) == 0) {
+			return true;
+		}
+	}
+	return false;
+}
+
+static void myTypeCommands(char *str, char commands[][16], int commandsSize) {
+	for (int i = 0; i < commandsSize; i++) {
+		if (strcmp(str, commands[i]) == 0) {
+			printf("%s is a shell builtin\n", str);
+			return;
+		}
+	}
+}
+
+static bool myTypeFileCheck(char *str) {
+	char *path = getenv("PATH");
+	int pathLength = strlen(path);
+	char *envPath = malloc(pathLength * sizeof(char));
+	strcpy(envPath, path);
+	char *dirpath = strtok(envPath, ":");
+	while (dirpath != NULL) {
+		DIR *directory = opendir(dirpath);
+		if (directory == NULL) {
+			dirpath = strtok(NULL, ":");
+			continue;
+		}
+		struct dirent *file;
+		while ((file = readdir(directory)) != NULL) {
+			if (strcmp(file->d_name, str) == 0) {
+				free(envPath);
+				closedir(directory);
+				return true;
+			}
+		}
+		dirpath = strtok(NULL, ":");
+		closedir(directory);
+	}
+	free(envPath);
+	return false;
+}
+
+static void myTypeFile(char *str) {
+	char *path = getenv("PATH");
+	int pathLength = strlen(path);
+	char *envPath = malloc(pathLength * sizeof(char));
+	strcpy(envPath, path);
+	char *dirpath = strtok(envPath, ":");
+	while (dirpath != NULL) {
+		DIR *directory = opendir(dirpath);
+		if (directory == NULL) {
+			dirpath = strtok(NULL, ":");
+			continue;
+		}
+		struct dirent *file;
+		while ((file = readdir(directory)) != NULL) {
+			if (strcmp(file->d_name, str) == 0) {
+				printf("%s is %s/%s\n", str, dirpath, file->d_name);
+				free(envPath);
+				closedir(directory);
+				return;
+			}
+		}
+		dirpath = strtok(NULL, ":");
+		closedir(directory);
+	}
+	free(envPath);
+	printf("%s not found\n", str);
+	return;
 }
